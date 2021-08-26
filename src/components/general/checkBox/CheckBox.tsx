6502c167f945e92723bef300a521d2af7e30f2f7
tristanwindham <tristanwindham.com>
2021-08-26 14:41:06 -0700

    Made the check svg's color dynamic. No longer switching between svgs

--- src/components/general/checkBox/CheckBox.tsx
+++ src/components/general/checkBox/CheckBox.tsx
@@ -1,9 +1,9 @@
-import React, { Dispatch, SetStateAction } from 'react'
-import checkIcon from './check.svg'
-import checkIconWhite from './check-white.svg'
+import React, { Dispatch, SetStateAction, useState } from 'react'
+import { ReactComponent as CheckIcon } from './check.svg'
 
 import './CheckBox.scss'
 
+// background-image: url(${(props) => props.src});
 const CheckBox = (props: CheckBoxProps) => {
   const { isChecked, setIsChecked } = props
 
@@ -14,11 +14,7 @@ const CheckBox = (props: CheckBoxProps) => {
         setIsChecked(!isChecked)
       }}
     >
-      <img
-        className="check"
-        src={isChecked ? checkIconWhite : checkIcon}
-        alt={`Checkmark ${isChecked}`}
-      />
+      <CheckIcon className="check" fill={isChecked ? 'white' : 'black'} />
     </div>
   )
 }
